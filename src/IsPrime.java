e25b475e5aa3b1e920d16762979c6e138286309b
somuknight <46134066+somuknight@users.noreply.github.com>
2020-07-20 19:17:52 +0530

    Check commit

--- src/IsPrime.java
+++ src/IsPrime.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class IsPrime {
+    static boolean isPrime(int n){
+
+        return  true;
+    }
     public static void main(String[] args) {
 
+        Scanner sc=new Scanner(System.in);
+        int a =sc.nextInt();
+        System.out.println(isPrime(a));
     }
 }
