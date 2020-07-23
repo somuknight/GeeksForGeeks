342589e468c09d321246c2b8b28c33a3ca531ec5
somuknight <46134066+somuknight@users.noreply.github.com>
2020-07-23 11:49:22 +0530

    Longest Distinct characters in string programme added

---
+++ src/Longest_Distinct_characters_in_string.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Longest_Distinct_characters_in_string {
+    static int LDCS(String str){
+        int n=0;
+
+        return n;
+    }
+    public static void main(String[] args) {
+        // TODO: 23-Jul-20 do the code in the branch
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int n=LDCS(str);
+    }
+}
