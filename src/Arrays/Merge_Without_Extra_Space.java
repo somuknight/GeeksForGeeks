ebb8762c4051ca5c664f36be88382a15a5cad00a
somuknight <46134066+somuknight@users.noreply.github.com>
2020-07-25 16:33:27 +0530

    initial commit

---
+++ src/Arrays/Merge_Without_Extra_Space.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+
+
+public class Merge_Without_Extra_Space {
+    public static void main(String[] args) {
+        int[] arr1={2, 3, 8, 13};
+        int[] arr2={1, 5, 9, 10, 15, 20};
+
+        for(int i=arr1.length-1,j=arr2.length-1;i>=0;i--,j--){
+            if(arr1[arr1.length-1]>=arr2[arr2.length-1]){
+
+            }
+
+        }
+    }
+}
