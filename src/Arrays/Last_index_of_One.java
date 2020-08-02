011bf3d9f902df70af2ac19c35b027978e6a9281
somuknight <46134066+somuknight@users.noreply.github.com>
2020-08-02 20:39:18 +0530

    nothing serious

--- src/Arrays/Last_index_of_One.java
+++ src/Arrays/Last_index_of_One.java
@@ -3,7 +3,6 @@ package Arrays;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Last_index_of_One {
