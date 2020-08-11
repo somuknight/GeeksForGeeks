c4aa174ada09c68773d854232bc63c6ee04a191f
somuknight <46134066+somuknight@users.noreply.github.com>
2020-08-11 14:21:29 +0530

    Stock_buy_and_sell program added...

---
+++ src/Arrays/Stock_buy_and_sell.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+
+
+public class Stock_buy_and_sell {
+    private static void buy_sell(int[] arr, int l){
+
+
+
+
+    }
+    public static void main(String[] args) {
+        int[] arr={100 ,50 ,30 ,20};
+        buy_sell(arr,arr.length);
+    }
+}
